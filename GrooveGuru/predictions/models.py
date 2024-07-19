96cac9f43de093c26cd2525c1f6f510794ee3e42
Deepa Tendulkar <149468101+deepatendulkar@users.noreply.github.com>
2024-07-19 17:14:21 +0530

    Update models.py

--- GrooveGuru/predictions/models.py
+++ GrooveGuru/predictions/models.py
@@ -1,15 +1,16 @@
+# predictions/models.py
 from django.db import models
 
 class AudioFile(models.Model):
     file = models.FileField(upload_to='audio/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
-# models.py
-from django.db import models
 
 class SongRecommendation(models.Model):
     title = models.CharField(max_length=200)
     artist = models.CharField(max_length=200)
     genre = models.CharField(max_length=100)
+    youtube_link = models.URLField(max_length=200, blank=True, null=True)
+
 
     def __str__(self):
-        return f"{self.title} by {self.artist}"
+        return f"{self.title} by {self.artist} - {self.youtube_link}"
